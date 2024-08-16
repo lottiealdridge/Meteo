b3eca3119f19d2537a9dfabe7261d04429642b0f
lottiealdridge <lottiealdridge95@gmail.com>
2024-08-16 13:34:16 +0100

    from JS added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
+
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit",handleSearchSubmit);
