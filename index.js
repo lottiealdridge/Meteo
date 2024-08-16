0a09ff30f51b443b7e23ac312c12a6d415986b17
lottiealdridge <lottiealdridge95@gmail.com>
2024-08-16 13:59:45 +0100

    Added API search functionality

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+
+function searchCity(city) {
+    let apiKey = "993a6b7650e8d3940tof6da3ca59d8f7";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+    //make api call
+    // update interface
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+searchCity("Paris");
