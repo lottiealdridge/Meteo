dcce7bc9b8bc872479b63fe189010f3847952fd9
lottiealdridge <lottiealdridge95@gmail.com>
2024-08-18 14:50:11 +0100

    updated date js

--- index.js
+++ index.js
@@ -6,23 +6,41 @@ function refreshWeather(response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
+    let date = new Date(response.data*1000);
 
-    console.log(response.data);
+    console.log(response.data.time);
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"];
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}:${minutes}`;
+
+
+}
 
 function searchCity(city) {
     let apiKey = "993a6b7650e8d3940tof6da3ca59d8f7";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
-    //make api call
-    // update interface
 }
 
 
